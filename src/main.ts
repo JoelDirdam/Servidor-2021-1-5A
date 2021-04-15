204fb200f3f9e3895d8f09684273fa1e67b1ce92
Joel Madrid <andres.javm00@gmail.com>
2021-04-14 22:19:56 -0500

    commiiiit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   //await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
