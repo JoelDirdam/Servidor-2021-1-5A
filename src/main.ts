3a60f924ba579369d7e14f627dcc79e35a8522e3
Joel Madrid <andres.javm00@gmail.com>
2021-04-15 18:11:57 -0500

    otro commit

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //await app.listen(3000);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT || 8080);
 }
