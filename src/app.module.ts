17a552bc6ce962ef90c2ed3f44a2da01d1ce10e8
Joel Madrid <andres.javm00@gmail.com>
2021-04-15 18:16:59 -0500

    mmmmmm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,12 @@ import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CategoriesModule } from './categories/categories.module';
 
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'node:path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UsuariosModule,
     CategoriesModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'avatars')
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
